472414a5c0d461d771e26116a9b072b957325fd8
Arvin <arvingrep@gmail.com>
2026-07-15 00:16:50 +0800

    fix: grant ec2:DisassociateAddress for NAT Gateway EIP teardown
    Found via a real terraform destroy: releasing the NAT Gateway's
    Elastic IP failed with AccessDenied on ec2:DisassociateAddress,
    which was never exercised by any prior apply (only teardown hits it).
    Same resource="*" pattern as the other EC2Networking actions already
    granted (AllocateAddress/ReleaseAddress).

--- modules/aws-iam-hcp/main.tf
+++ modules/aws-iam-hcp/main.tf
@@ -89,7 +89,7 @@ data "aws_iam_policy_document" "lab_permissions" {
       "ec2:CreateInternetGateway", "ec2:DeleteInternetGateway", "ec2:AttachInternetGateway",
       "ec2:DetachInternetGateway", "ec2:DescribeInternetGateways",
       "ec2:CreateNatGateway", "ec2:DeleteNatGateway", "ec2:DescribeNatGateways",
-      "ec2:AllocateAddress", "ec2:ReleaseAddress", "ec2:DescribeAddresses", "ec2:DescribeAddressesAttribute",
+      "ec2:AllocateAddress", "ec2:ReleaseAddress", "ec2:DisassociateAddress", "ec2:DescribeAddresses", "ec2:DescribeAddressesAttribute",
       "ec2:CreateRouteTable", "ec2:DeleteRouteTable", "ec2:CreateRoute", "ec2:DeleteRoute", "ec2:ReplaceRoute",
       "ec2:AssociateRouteTable", "ec2:DisassociateRouteTable", "ec2:DescribeRouteTables",
       "ec2:CreateSecurityGroup", "ec2:DeleteSecurityGroup", "ec2:DescribeSecurityGroups", "ec2:DescribeSecurityGroupRules",
